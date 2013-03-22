776be8fe2ee51466ab310162a09b0037f1b2f202
guoplus <kaqimu@foxmail.com>
2013-03-22 13:37:28 +0800

    edit gflib

--- js/gflib.js
+++ js/gflib.js
@@ -1,21 +1,23 @@
-/*
-*addLoadEvent(func);
-*添加页面加载完毕时要执行函数。
-*每个事件处理函数只能绑定一条命令。
-*window.onload只能执行最后绑定的函数，
-*此函数提供了一个加载完毕执行任意条函数的解决方案。
-*eg.addLoadEvent(abc);
-*http://simon.incutio.com
-*09-08-13
-*/
-function addLoadEvent(func){
-  var oldonload=window.onload;
-  if(typeof window.onload!='function'){
-    window.onload=func;
-  }else{
-    window.onload=function(){
-	  oldonload();
-	  func();
-	}
-  }
-}
+
+var gflib = gflib || {};
+
+/**
+ * 为window.onload事件添加方法
+ * window.onload只能执行最后绑定的函数
+ * 此函数提供了一个加载完毕执行任意条函数的解决方案。
+ * eg.addLoadEvent(abc);
+ * http://simon.incutio.com
+ * 09-08-13
+ */
+gflib.ready = function(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload=func;
+    }
+    else{
+        window.onload = function(){
+            oldonload();
+            func();
+        };
+    }
+};
