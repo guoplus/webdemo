0d0134b107b80b4e6e6f7deeb4bcb67831db7fad
guoplus <kaqimu@foxmail.com>
2013-03-22 13:16:03 +0800

    add gflib edit dropmenu

---
+++ js/gflib.js
@@ -0,0 +1,21 @@
+/*
+*addLoadEvent(func);
+*添加页面加载完毕时要执行函数。
+*每个事件处理函数只能绑定一条命令。
+*window.onload只能执行最后绑定的函数，
+*此函数提供了一个加载完毕执行任意条函数的解决方案。
+*eg.addLoadEvent(abc);
+*http://simon.incutio.com
+*09-08-13
+*/
+function addLoadEvent(func){
+  var oldonload=window.onload;
+  if(typeof window.onload!='function'){
+    window.onload=func;
+  }else{
+    window.onload=function(){
+	  oldonload();
+	  func();
+	}
+  }
+}
--- old/js/addLoadEvent.js
+++ old/js/addLoadEvent.js
@@ -18,4 +18,4 @@ function addLoadEvent(func){
 	  func();
 	}
   }
-}/*addLoadEvent() end*/
+}
